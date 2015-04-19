00d1a7c85d25724487100a3b9b95d1affc87b49c
Alexander Zaytsev <hazzik@gmail.com>
2015-04-20 11:49:38 +1200

    Set version 1.0.5 Update copyright year

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Brandy")]
 [assembly: AssemblyProduct("Brandy.Grapes")]
-[assembly: AssemblyCopyright("Copyright © \"Brandy Solutions Ltd\" 2012 - 2014")]
+[assembly: AssemblyCopyright("Copyright © \"Brandy Solutions Ltd\" 2012 - 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
